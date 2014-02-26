cb460442de61f6b243a6d1724d6c3ef362c922b1
flashback <klseb@hotmail.de>
2014-02-26 01:37:10 +0100

    add verwendete Bibo Dialog

--- src/com/flashback/yeahmatrix/StartActivity.java
+++ src/com/flashback/yeahmatrix/StartActivity.java
@@ -1,19 +1,26 @@
 package com.flashback.yeahmatrix;
 
-import com.readystatesoftware.systembartint.SystemBarTintManager;
-
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.readystatesoftware.systembartint.SystemBarTintManager;
+
 
 public class StartActivity extends Activity {
 
+	final Context context = this;
 	private Button btnZweierMatrix, btnDreierMatrix;
-	
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -49,6 +56,55 @@ public class StartActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.start, menu);
-		return false;
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+	       case R.id.action_settings:
+	    	   openLicenseDialog();
+	           return true;
+	       default:
+	           return super.onOptionsItemSelected(item);
+	    }
+	}
+	
+	public void openLicenseDialog() {
+		//
+		final Dialog dialog = new Dialog(context);
+		dialog.setContentView(R.layout.licensedialog);
+		dialog.setTitle("verwendete Bibliotheken");
+		//
+		TextView tv1 = (TextView)dialog.findViewById(R.id.tvCrouton);
+		tv1.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent i1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/keyboardsurfer/Crouton"));
+				startActivity(i1);
+			}
+		});
+		//
+		TextView tv2 = (TextView)dialog.findViewById(R.id.tvSystemBarTint);
+		tv2.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent i2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/jgilfelt/SystemBarTint"));
+				startActivity(i2);
+			}
+		});
+		//
+		Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+		// if button is clicked, close the custom dialog
+		dialogButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog.dismiss();
+			}
+		});
+		//
+		dialog.show();
 	}
 }
